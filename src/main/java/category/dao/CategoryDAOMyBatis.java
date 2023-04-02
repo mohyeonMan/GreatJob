61c0d7e27a5ee94cdd17f7447714e248be7e2d80
mohyeonMan <112916714+mohyeonMan@users.noreply.github.com>
2023-04-02 19:27:21 +0900

    CategoryDAOMyBatis contstructor

--- src/main/java/category/dao/CategoryDAOMyBatis.java
+++ src/main/java/category/dao/CategoryDAOMyBatis.java
@@ -5,11 +5,16 @@ import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.RequiredArgsConstructor;
+
 @Repository
+@Primary
 @Transactional
+@RequiredArgsConstructor
 public class CategoryDAOMyBatis {
 	@Autowired
 	SqlSession sqlSession;
