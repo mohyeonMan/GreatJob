f32dbb164f5bc95091a07de82ae7d21317ed8ae4
mohyeonMan <112916714+mohyeonMan@users.noreply.github.com>
2023-04-03 12:32:57 +0900

    resultMap create

--- src/main/java/recruit/bean/RecruitDTO.java
+++ src/main/java/recruit/bean/RecruitDTO.java
@@ -1,6 +1,5 @@
 package recruit.bean;
 
-import java.sql.Timestamp;
 
 import lombok.Data;
 
@@ -12,17 +11,17 @@ public class RecruitDTO {
 	private String description;
 	private int categoryId;
 	private String categoryName;
-	private Timestamp dateStart;
-	private Timestamp dateEnd;
+	private long dateStart;
+	private long dateEnd;
 	private String host;
 	private int userId;
 	private String userName;
 	private int maxPersonnel;
 	private int currentPersonnel;
 	private String address;
-	private Timestamp dateCreated;
-	private Timestamp dateModified;
-
+	private long dateCreated;
+	private long dateModified;
+	
 //	public long getDateStart() {
 //		return dateStart.getTime();
 //	}
