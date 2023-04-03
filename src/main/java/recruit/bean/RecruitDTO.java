d3efecb8992f3413096dd28fadbadfa0993428f5
mohyeonMan <112916714+mohyeonMan@users.noreply.github.com>
2023-04-03 11:00:55 +0900

    RecruitDTO DateType to timestamp

--- src/main/java/recruit/bean/RecruitDTO.java
+++ src/main/java/recruit/bean/RecruitDTO.java
@@ -1,26 +1,41 @@
 package recruit.bean;
 
-import java.sql.Date;
-
+import java.sql.Timestamp;
 
 import lombok.Data;
 
 @Data
 public class RecruitDTO {
-	
+
 	private int id;
 	private String title;
 	private String description;
 	private int categoryId;
 	private String categoryName;
-	private Date dateStart;
-	private Date dateEnd;
+	private Timestamp dateStart;
+	private Timestamp dateEnd;
 	private String host;
 	private int userId;
 	private String userName;
 	private int maxPersonnel;
 	private int currentPersonnel;
 	private String address;
-	private Date dateCreated;
-	private Date dateModified;
+	private Timestamp dateCreated;
+	private Timestamp dateModified;
+
+	public long getDateStart() {
+		return dateStart.getTime();
+	}
+
+	public long getDateEnd() {
+		return dateEnd.getTime();
+	}
+
+	public long getDateCreated() {
+		return dateCreated.getTime();
+	}
+
+	public long getDateModified() {
+		return dateModified.getTime();
+	}
 }
