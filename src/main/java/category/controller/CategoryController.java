e3e7c6fb0c703cd57725e1f4e5b3d6b6c1697f8d
mohyeonMan <112916714+mohyeonMan@users.noreply.github.com>
2023-04-04 21:48:50 +0900

    C:/Program Files/Git/category return status

--- src/main/java/category/controller/CategoryController.java
+++ src/main/java/category/controller/CategoryController.java
@@ -1,8 +1,10 @@
 package category.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,13 +25,24 @@ public class CategoryController {
 	private CategoryDAOMyBatis dao;
 	
 	@PostMapping(value = "create")
-	public void create(@RequestBody Map<String, Object> map) {
+	public String create(@RequestBody Map<String, Object> map) {
 		dao.create((String)map.get("name"));
+		return new JSONObject().put("status", 200).toString();
 	}
 	
 	@GetMapping(value = "listCategories")
 	public String listCategories() {
-		JSONArray arr = new JSONArray(dao.listCategories());
-		return arr.toString();
+		JSONObject object = new JSONObject();
+		
+		List<Map<String, Object>> arr = dao.listCategories();
+		
+		if(arr == null) {
+			object.put("status", 500);
+		}else {
+			object.put("categories",new JSONArray(arr));
+			object.put("status", 200);
+		}
+		
+		return object.toString();
 	}
 }
