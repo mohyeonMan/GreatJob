01c8a89f60342b17c26166927a18435c255eee20
mohyeonMan <112916714+mohyeonMan@users.noreply.github.com>
2023-04-15 16:03:38 +0900

    recruitDTO "rn"field added

--- src/main/java/recruit/bean/RecruitDTO.java
+++ src/main/java/recruit/bean/RecruitDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class RecruitDTO {
 
 	private int id;
-	private int row;
+	private int rn;
 	private String title;
 	private String description;
 	private int categoryId;
