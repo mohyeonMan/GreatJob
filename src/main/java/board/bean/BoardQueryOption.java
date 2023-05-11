727773700e344b8820ca30752a4224cefe16e4cc
mohyeonMan <112916714+mohyeonMan@users.noreply.github.com>
2023-05-12 00:03:53 +0900

    listBoardService

--- src/main/java/board/bean/BoardQueryOption.java
+++ src/main/java/board/bean/BoardQueryOption.java
@@ -1,5 +1,24 @@
 package board.bean;
 
-public class BoardQueryOption {
+import java.util.ArrayList;
+import java.util.List;
 
+import lombok.Data;
+
+@Data
+public class BoardQueryOption {
+	
+	private List<Integer> categoryId;
+	private String sort;
+	private int startItem = 0;
+	private int itemAmount= 10;
+	
+	public void setCategoryId(String categoryId) {
+		String[] strArr = categoryId.split(",");
+		List<Integer> list = new ArrayList<>();
+		for (String str : strArr) {
+			list.add(Integer.parseInt(str));
+		}
+		this.categoryId = list;
+	}
 }
--- src/main/java/board/service/ListBoardsService.java
+++ src/main/java/board/service/ListBoardsService.java
@@ -1,5 +1,57 @@
 package board.service;
 
-public class ListBoardsService {
+import java.util.List;
+import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import board.bean.BoardDTO;
+import board.bean.BoardQueryOption;
+import board.dao.BoardDAO;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ListBoardsService implements BoardService{
+	@Autowired
+	private Map<String, BoardDAO> boardDAO;
+	
+	@Override
+	public String execute(Map<String, Object> map) {
+		BoardDAO dao = boardDAO.get("boardDAOMyBatis");
+		JSONObject object = new JSONObject();
+		
+		BoardQueryOption option = parseValue(map);
+		
+		List<BoardDTO> boards = dao.listBoards(option);
+		object.put("data", new JSONArray(boards));
+		object.put("status", 200);
+		
+		return object.toString();
+	}
+	
+	private BoardQueryOption parseValue(Map<String, Object> map) {
+		BoardQueryOption option = new BoardQueryOption();
+		if(map != null) {
+			if(map.get("categoryId") != null) {
+				option.setCategoryId((String)map.get("categoryId"));				
+			}
+			if(map.get("sort") != null) {
+				option.setSort((String)map.get("sort")); 
+			}
+			if (map.get("startItem") != null) {
+				option.setStartItem(Integer.parseInt((String)map.get("startItem")));
+			}
+			if (map.get("itemAmount") != null) {
+				option.setItemAmount(Integer.parseInt((String)map.get("itemAmount")));
+			}
+		}
+		
+		return option;
+	}
+
+	
 }
